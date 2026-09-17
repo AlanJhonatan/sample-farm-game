3f115f3f574a5ce6b8695c7b8170d47076beb2af
AlanJhonatan <alan.amorim.franca@gmail.com>
2026-09-17 15:07:28 -0300

    feat: enable pixel-art optimization

--- src/game/config.ts
+++ src/game/config.ts
@@ -6,12 +6,13 @@ const config: Phaser.Types.Core.GameConfig = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
+  pixelArt: true,
 
   physics: {
     default: "arcade",
     arcade: {
       gravity: { x: 0, y: 0 },
-      debug: true,
+      // debug: true,
     },
   },
 
