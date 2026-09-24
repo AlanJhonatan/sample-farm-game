6a6d6aaf928853759a09488a98e6db0ea5deb9c2
AlanJhonatan <alan.amorim.franca@gmail.com>
2026-09-24 17:15:45 -0300

    feat: implement tilesets variants based on treshold

--- src/game/_terrain/managers/TerrainTilemapManager.ts
+++ src/game/_terrain/managers/TerrainTilemapManager.ts
@@ -1,13 +1,26 @@
 import * as Phaser from "phaser";
+import type { TerrainNoisemapGenerator } from "../generators/TerrainNoisemapGenerator";
+import { tresholdHelper } from "../helpers/TresholdHelper";
 import type { TerrainTilesetLoader } from "../loaders/TerrainTilesetLoader";
-import type { GrassTileVariant } from "../manifests/Tilesets";
+import { type TerrainType } from "../manifests/TerrainTreshold";
+import type {
+  GrassTileVariant,
+  SandTileVariant,
+  StoneTileVariant,
+  WaterTileVariant,
+} from "../manifests/Tilesets";
 import { TerrainTilesetManager } from "./TerrainTilesetManager";
 
 interface TilemapManagerProps {
   scene: Phaser.Scene;
 }
 
-export type TilemapLayers = "ground" | "tree";
+interface TerrainVariantMap {
+  grass: GrassTileVariant;
+  water: WaterTileVariant;
+  stone: StoneTileVariant;
+  sand: SandTileVariant;
+}
 
 export class TerrainTilemapManager {
   private scene: Phaser.Scene;
@@ -15,68 +28,24 @@ export class TerrainTilemapManager {
   private tilemap: Phaser.Tilemaps.Tilemap;
   private tilesetManager: TerrainTilesetManager;
   private tilesetLoader: TerrainTilesetLoader;
+  private terrainNoisemap: TerrainNoisemapGenerator;
+
+  private terrainmap: Map<string, TerrainType> = new Map();
 
   constructor({ scene }: TilemapManagerProps) {
     this.scene = scene;
   }
 
-  preload() {}
-
   register(
+    tilemap: Phaser.Tilemaps.Tilemap,
     tilesetManager: TerrainTilesetManager,
     tilesetLoader: TerrainTilesetLoader,
+    terrainNoisemap: TerrainNoisemapGenerator,
   ) {
+    this.tilemap = tilemap;
     this.tilesetManager = tilesetManager;
     this.tilesetLoader = tilesetLoader;
-  }
-
-  create() {
-    this.tilemap = this.scene.make.tilemap({
-      tileWidth: 16,
-      tileHeight: 16,
-      width: 16,
-      height: 16,
-    });
-
-    const tilesetKeys = this.tilesetLoader.getTilesetKeys();
-
-    tilesetKeys.forEach((key) => {
-      const { size } = this.tilesetLoader.getTilesetInfo(key);
-
-      return this.tilemap.addTilesetImage(
-        key,
-        key,
-        size,
-        size,
-        0,
-        0,
-        this.getTilesetGID(),
-      );
-    });
-
-    this.tilemap.createBlankLayer("ground", tilesetKeys).putTileAt(0, 0, 0);
-    this.putCustomTileAt("grass", 1, 1, "ground");
-  }
-
-  update() {}
-
-  putCustomTileAt(
-    tileVariant: GrassTileVariant,
-    x: number,
-    y: number,
-    layerName: TilemapLayers,
-  ) {
-    const tile = this.tilesetManager.getGrassTile("grass", tileVariant);
-
-    const localgid = this.tilesetManager.tileToLocalGID(tile);
-
-    const tilesetIdx = this.tilemap.getTilesetIndex(tile.key);
-    const tileset = this.tilemap.tilesets[tilesetIdx];
-
-    const firstgid = tileset.firstgid;
-    const gid = firstgid + localgid;
-
-    this.tilemap.putTileAt(gid, x, y, false, layerName);
+    this.terrainNoisemap = terrainNoisemap;
   }
 
   getTilemap() {
@@ -86,6 +55,69 @@ export class TerrainTilemapManager {
   getTilesetGID() {
     if (!this.tilemap.tilesets.length) return 0;
 
-    return this.tilemap.tilesets.at(-1).total;
+    const lastTileset = this.tilemap.tilesets.at(-1);
+
+    const lastgid = lastTileset.firstgid;
+    const lastTotal = lastTileset.total - 1;
+
+    return lastgid + lastTotal;
+  }
+
+  create() {
+    const tilesetKeys = this.tilesetLoader.getTilesetKeys();
+
+    tilesetKeys.forEach((key) => {
+      const { size } = this.tilesetLoader.getTilesetInfo(key);
+      const gid = this.getTilesetGID();
+
+      return this.tilemap.addTilesetImage(key, key, size, size, 0, 0, gid);
+    });
+
+    this.tilemap.createBlankLayer("ground", tilesetKeys);
+  }
+
+  putTerrainTileAt<T extends TerrainType>(
+    terrainType: T,
+    tileVariant: TerrainVariantMap[T],
+    x: number,
+    y: number,
+  ) {
+    const tile = this.tilesetManager.getTileFromTileset(
+      terrainType,
+      tileVariant,
+    );
+
+    const localgid = this.tilesetManager.tileToLocalGID(tile);
+
+    const tilesetIdx = this.tilemap.getTilesetIndex(tile.key);
+    const tileset = this.tilemap.tilesets[tilesetIdx];
+
+    const firstgid = tileset.firstgid;
+    const gid = firstgid + localgid;
+
+    this.tilemap.putTileAt(gid, x, y, false, "ground");
+  }
+
+  generateTerrainFromNoise() {
+    const rows = this.tilemap.height;
+    const cols = this.tilemap.width;
+
+    for (let y = 0; y < rows; y++) {
+      for (let x = 0; x < cols; x++) {
+        const noise = this.terrainNoisemap.getNoisemapAt(x, y);
+
+        const type = tresholdHelper.tilesetFromTreshold(noise);
+
+        this.terrainmap.set(`[${x},${y}]`, type);
+
+        this.putTerrainTileAt(type, "base", x, y);
+      }
+    }
+  }
+
+  getTerrainTypeAt(position: { x: number; y: number }): TerrainType {
+    const idx = `[${position.x},${position.y}]`;
+
+    return this.terrainmap.get(idx);
   }
 }
--- src/game/_terrain/managers/TerrainTilesetManager.ts
+++ src/game/_terrain/managers/TerrainTilesetManager.ts
@@ -1,23 +1,49 @@
-import { GrassTile } from "../entities/GrassTile";
-import type { Tile } from "../entities/Tile";
+import {
+  GrassTile,
+  SandTile,
+  StoneTile,
+  WaterTile,
+  type Tile,
+} from "../entities/Tile";
+import type { TerrainType } from "../manifests/TerrainTreshold";
 import {
   GRASS_TILES,
+  SAND_TILES,
+  STONE_TILES,
   TILESETS_CONFIG,
+  WATER_TILES,
   type GrassTileVariant,
+  type SandTileVariant,
+  type StoneTileVariant,
   type TilesetAsset,
+  type WaterTileVariant,
 } from "../manifests/Tilesets";
 
-export type Tilesets = "grass";
+interface TilesetVariantMap {
+  grass: GrassTileVariant;
+  water: WaterTileVariant;
+  stone: StoneTileVariant;
+  sand: SandTileVariant;
+}
 
 export class TerrainTilesetManager {
   private grassTiles: Map<GrassTileVariant, GrassTile> = new Map();
+  private waterTiles: Map<WaterTileVariant, WaterTile> = new Map();
+  private stoneTiles: Map<StoneTileVariant, StoneTile> = new Map();
+  private sandTiles: Map<SandTileVariant, SandTile> = new Map();
 
-  private tileset: Map<Tilesets, Map<string, Tile>> = new Map();
+  private tileset: Map<TerrainType, Map<string, Tile>> = new Map();
 
   constructor() {
     this.registerGrassTiles();
+    this.registerWaterTiles();
+    this.registerStoneTiles();
+    this.registerSandTiles();
 
     this.tileset.set("grass", this.grassTiles);
+    this.tileset.set("water", this.waterTiles);
+    this.tileset.set("stone", this.stoneTiles);
+    this.tileset.set("sand", this.sandTiles);
   }
 
   private registerGrassTiles() {
@@ -38,7 +64,64 @@ export class TerrainTilesetManager {
     });
   }
 
-  getGrassTile(tilesetName: Tilesets, variant: GrassTileVariant) {
+  registerWaterTiles() {
+    const entries = Object.entries(WATER_TILES);
+
+    entries.forEach(([key, tiles]) => {
+      tiles.forEach((tile) => {
+        const water = new WaterTile({
+          key,
+          variant: tile.variant,
+          x: tile.x,
+          y: tile.y,
+          tilesetName: "water",
+        });
+
+        this.waterTiles.set(tile.variant, water);
+      });
+    });
+  }
+
+  registerStoneTiles() {
+    const entries = Object.entries(STONE_TILES);
+
+    entries.forEach(([key, tiles]) => {
+      tiles.forEach((tile) => {
+        const stone = new StoneTile({
+          key,
+          variant: tile.variant,
+          x: tile.x,
+          y: tile.y,
+          tilesetName: "stone",
+        });
+
+        this.stoneTiles.set(tile.variant, stone);
+      });
+    });
+  }
+
+  registerSandTiles() {
+    const entries = Object.entries(SAND_TILES);
+
+    entries.forEach(([key, tiles]) => {
+      tiles.forEach((tile) => {
+        const sand = new SandTile({
+          key,
+          variant: tile.variant,
+          x: tile.x,
+          y: tile.y,
+          tilesetName: "sand",
+        });
+
+        this.sandTiles.set(tile.variant, sand);
+      });
+    });
+  }
+
+  getTileFromTileset<T extends TerrainType>(
+    tilesetName: T,
+    variant: TilesetVariantMap[T],
+  ) {
     const tileset = this.tileset.get(tilesetName);
     return tileset.get(variant);
   }
